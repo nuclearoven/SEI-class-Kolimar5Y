6f465bdc4d78f04735722acf373804ca21563053
Jakub Kolimar <kolimar.jakub@gmail.com>
2025-11-12 08:14:31 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -1,27 +1,23 @@
-year = int(input("Enter year: "))
+def substract(a, b):
+    return a - b
+print(substract(3, 5))
 
-a = year % 19
-b = year % 4
-c = year % 7
+def multiply(a, b):
+    return a * b
+print(multiply(3, 5))
 
-k = year // 100
-p = (13 + 8 * k) // 25  # fixed (1816), was: k // 3
-q = k // 4
-M = (15 - p + k - q) % 30
-N = (4 + k - q) % 7
+def divide(a, b):
+    return a / b
+print(divide(3, 5))
 
-d = (19 * a + M) % 30
-e = (2 * b + 4 * c + 6 * d + N) % 7
-march_easter = d + e + 22
-april_easter = d + e - 9
+def floordivide(a, b):
+    return a // b
+print(floordivide(3, 5))
 
-if april_easter == 25 and d == 28 and e == 6 and a > 10:  # changed (1807), was: (11 * M + 11) % 30 < 19
-    april_easter = 18
+def modulus(a, b):
+    return a % b
+print(modulus(3, 5))
 
-if april_easter == 26 and d == 29 and e == 6:
-    april_easter = 19
-
-if march_easter <= 31:
-    print(march_easter, "March")
-else:
-    print(april_easter, "April")
+def power(a, b):
+    return pow(a, b)
+print(power(3, 5))
