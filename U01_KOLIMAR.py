4640bdf9a0823b871ff8e977f0f49e52ea443211
Jakub Kolimar <kolimar.jakub@gmail.com>
2025-11-18 19:58:01 +0100

    Delete U01_KOLIMAR.py

--- U01_KOLIMAR.py
+++
@@ -1,8 +0,0 @@
-file = open('Numbers.txt', 'r')
-text = file.readlines()
-divisor = int(input("Enter divisor"))
-i=0
-for x in text:
-    i=i+1
-    if int(text[i-1]) % divisor == 0:
-        print(x)
